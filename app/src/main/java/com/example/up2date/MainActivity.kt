7a878ac3a67d31033fbe8d4a06f5a1fc92f15d2c
Yash Raj Singh <yashrajsingh807@gmail.com>
2021-11-29 11:02:54 +0530

    Updated Volley to make it work with NewsAPI.org API

--- app/src/main/java/com/example/up2date/MainActivity.kt
+++ app/src/main/java/com/example/up2date/MainActivity.kt
@@ -28,29 +28,40 @@ class MainActivity : AppCompatActivity(), newsItemClicked {
     }
 
     private fun fetchData(){
-        val url = "https://saurav.tech/NewsAPI/everything/cnn.json"
-        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
-                Response.Listener {
-                    val newsJsonArray = it.getJSONArray("articles")
-                    val newsArray = ArrayList<NewsData>()
-                    for(i in 0 until newsJsonArray.length()){
-                        val newsJsonObject = newsJsonArray.getJSONObject(i)
-                        val news = NewsData(
-                                newsJsonObject.getString("title"),
-                                newsJsonObject.getString("author"),
-                                newsJsonObject.getString("url"),
-                                newsJsonObject.getString("urlToImage")
-                        )
-                        newsArray.add(news)
-                    }
 
-                    mAdapter.updatedNews(newsArray)
+        val url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=104dfe7bf6294d6687ba78c0cb327dd3"
 
-                },
-                Response.ErrorListener {
+        //Alternate API
+        //val url = "https://saurav.tech/NewsAPI/everything/cnn.json"
 
+        val jsonObjectRequest = object : JsonObjectRequest(Request.Method.GET, url, null,
+            Response.Listener {
+                val newsJsonArray = it.getJSONArray("articles")
+                val newsArray = ArrayList<NewsData>()
+                for(i in 0 until newsJsonArray.length()){
+                    val newsJsonObject = newsJsonArray.getJSONObject(i)
+                    val news = NewsData(
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("author"),
+                        newsJsonObject.getString("url"),
+                        newsJsonObject.getString("urlToImage")
+                    )
+                    newsArray.add(news)
                 }
-        )
+
+                mAdapter.updatedNews(newsArray)
+
+            },
+            Response.ErrorListener {
+
+            }
+        ) {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["User-Agent"] = "Mozilla/5.0"
+                return headers
+            }
+        }
 
 // Access the RequestQueue through your singleton class.
         MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
